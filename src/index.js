67c0b711a45555abdbd734ec480d0753c189391c
Victorino Machava <vitomachava@gmail.com>
2021-09-29 02:15:25 +0200

    Handle create user calls with empty payload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import http from 'http';
 
 function requestHandler(req, res) {
+  if (req.method === 'POST' && req.url === '/users') {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      message: 'Payload should not be empty',
+    }));
+    return;
+  }
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, Beautiful People!');
 }
